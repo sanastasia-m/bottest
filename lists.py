31015a57f7f83ed700faca83d62fc9c91e257a1f
sanastasia-m <52889759+sanastasia-m@users.noreply.github.com>
2021-07-03 21:20:32 +0300

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,99 @@
+if_cloudly = ['CAACAgIAAxkBAAEBMolggEyoar_OYneyrrtaGSDvoCfbKwACIgEAAqZESAsR9bnMjxX0Zh8E',
+              'CAACAgIAAxkBAAEBMo9ggE3vY5EpNqEd_vd3O3OIS5UDOwACDwIAAtzyqwflTv80MV32fh8E',
+              'CAACAgIAAxkBAAEBMoxggE3t3Go3LA-9yYvTCcD_IpiMSAACMQEAAjDUnREb_jFE4pMwhh8E']
+
+if_sunny_and_hot = ['CAACAgIAAxkBAAEBMqFggFUIpURaz5yWv6SEd2gkCx4eSgACiAADFkJrClvwoKH4eQABJR8E',
+            'CAACAgIAAxkBAAEBMp5ggFT7knXkzhnbV1alHV5nF2PAbgACgwEAAhZCawrJEQoL_-AV1R8E',
+            'CAACAgIAAxkBAAEBMptggFTv9nAvFj5upkKUSfx7UqdlTgACIAEAAhAhAhDImk3RaQXWQh8E',
+            'CAACAgIAAxkBAAEBMphggFTti3bqzNhHll-syHu25F8IsAACUQADr8ZRGvLAcjetPyfvHwQ',
+            'CAACAgIAAxkBAAEBMpVggFTnseptITtPfixt0F46Mo1-1wACBAUAAkf7CQx61hycJQfNDR8E',
+            'CAACAgIAAxkBAAEBMpJggFTmbYff8dHLRuZh_9kXnp5jtAACVQADrWW8FKBlcGGKRCXhHwQ']
+
+if_sunny = ['CAACAgIAAxkBAAEBMrNggHkXEW4tF1jHx8YapI8_PVBj1gACIQIAAtzyqwe0Fc_LsMF1aB8E',
+            'CAACAgIAAxkBAAEBMrlggHmHdfIydL5xC7EEjWU-AAEi4dsAAjYCAALPu9QOiHezvdE3SUcfBA',
+            'CAACAgIAAxkBAAEBMrZggHl_R7Gn48t7Yr5Kov5SgkVwhgACegEAAhZCawqYRZuYnxC0lx8E']
+
+if_rainy = ['CAACAgIAAxkBAAEBMr9ggHsY_4rZQF4AAcdy5axY_Ozbn-EAAmsBAAKmREgL0LIp7NiFVFkfBA',
+            'CAACAgIAAxkBAAEBMsJggHsc2SRuRXAR197Du4i1TGsnVwACPQEAAjDUnREQ98oHcZKP7h8E',
+            'CAACAgIAAxkBAAEBMsVggHsdjWJG9QEDRGEmjhPebqk8jgACEAUAAs9fiweKscZVYpoFIh8E',
+            'CAACAgIAAxkBAAEBMshggHsf0Y8JJ_cx_0WdsHDFbol2ggACHAIAAtzyqwcOMlNVtoZBtR8E',
+            'CAACAgIAAxkBAAEBMstggHslz1pZMChdIPbIWHTfF7EsgAACQQADwDZPE08eOxgnDpt2HwQ',
+            'CAACAgIAAxkBAAEBMs5ggHstEgoLh4z4hWBGTmL-MLEgMwACtgADnNbnCqRL_4alee3ZHwQ']
+
+if_snowy = ['CAACAgIAAxkBAAEBMtpggILNaeFxPsn2h0iAPMAjv4fg0AACQAADWbv8Jbm_QcU3AAFdGR8E',
+            'CAACAgIAAxkBAAEBMtdggILFsBb-fi1NKl0U_0z5kHfy1AACOAEAAqZESAvMWrzVi1b-6R8E',
+            'CAACAgIAAxkBAAEBMtRggIKwFJXYnBWDKBgfrzehr_pTAAMeCQACbjLYAAF2AylDZ9CQPh8E',
+            'CAACAgIAAxkBAAEBMtFggIKnXSwsMOvBR81eGwGAn5KATgACygUAAiMFDQABG-hgbSZuySAfBA',
+            'CAACAgIAAxkBAAEBMt1ggIMw_SpI5XO5gzeTEidrL5b7hAACaQADO2AkFLD52hFMye-XHwQ',
+            'CAACAgIAAxkBAAEBMuBggINIFBVC8h6alW1yMkRIK6VSbgAChgAD9wLIDw5AaN32E2B9HwQ']
+
+if_fogy = ['CAACAgIAAxkBAAEBMvRggIRTRQn34HifUZDLMibUeT01bgACCQcAApb6EgVKa4vBDATAGR8E',
+           'CAACAgIAAxkBAAEBMvFggIRLsvSrYOsDNHwAAcm3yo2XAAFoAAJ-AAPBnGAMCxR_3b0i_fMfBA',
+           'CAACAgIAAxkBAAEBMu5ggIRHVME1nOz6bKp6ezoFky9A_AACQgADwDZPE6AFnXskhiWOHwQ',
+           'CAACAgIAAxkBAAEBMutggIQ3Q0HVbjd0aBaEmZqK2vXWaAACRgEAAladvQpHSJ8FNCc7ZB8E',
+           'CAACAgIAAxkBAAEBMuhggIQ0KEXwhnE0NwWJk3Duojqa5wACRwEAAjDUnRGOQ5cS_6ydwR8E',
+           'CAACAgIAAxkBAAEBMuNggIPeD9OfSxk07svb4nWaiOIeeQACaQEAAqZESAt_3dxMvA3vCR8E']
+
+if_not = [
+    'CAACAgIAAxkBAAEBNJJghCXmP-UffKexgdwUp079EoVpCgACBAADO2AkFLOr61RvleGrHwQ',
+    'CAACAgIAAxkBAAEBNI9ghCXXnZHrL--OZo7UZA5F_11UyAACQgADWbv8Jd7Bb6A7P1vRHwQ',
+    'CAACAgIAAxkBAAEBNIxghCW_smX_i9cEpg6D_Yg0SCW1gQACKwIAAtzyqwf3oAkr2wnPBh8E',
+    'CAACAgIAAxkBAAEBNIlghCW6FBe3SsQlApA_4UdZeAcXLQACLQEAAtA0Oiegz8D9bZOOmh8E',
+    'CAACAgIAAxkBAAEBNIZghCW4YAU2ZInSNIfFknCizEA0LwACIQIAAtzyqwe0Fc_LsMF1aB8E',
+    'CAACAgIAAxkBAAEBNINghCW3hDRu1K52KXU2m9fFr4gCnQACNgIAAs-71A6Id7O90TdJRx8E',
+    'CAACAgIAAxkBAAEBNIBghCW0-S4HO8uSdtKaexkQpEODfAACegEAAhZCawqYRZuYnxC0lx8E',
+    'CAACAgIAAxkBAAEBNJ1ghCbLXhVYmX9f4zx4JHg9XSYIgAACGgAD9wLID68vCWlyjjbLHwQ',
+    'CAACAgIAAxkBAAEBNJpghCbHaVuQvhnCbqQ9CbaJ29qtMgACDQADWbv8JS5RHx3i_HUDHwQ',
+    'CAACAgIAAxkBAAEBNJdghCayzdLATBi-fFVY4ph28bHMyAACWwADoYxBC4cRG4cpb9PAHwQ'
+]
+
+if_yes = [
+    'конечно',
+    'действительно',
+    'так точно',
+    'правда',
+    'несомненно',
+    'точно',
+    'а как же',
+    'безусловно',
+    'есть такое дело',
+    'само собой разумеется'
+]
+
+if_no = [
+    'никак нет',
+    'ни в коем случае',
+    'ни за что на свете',
+    'ни за какие сокровища',
+    'я бы не сказала',
+    'вовсе нет',
+    'еще чего!',
+    'и не пахнет',
+    'отнють нет',
+    'ни за какие блага',
+    'еще чего захотел',
+    'ни в коем разе',
+    'ни за что',
+    'и помину нет',
+    'слыхом не слыхать',
+    'ничего подобного',
+    'не скажите',
+    'отсутствует',
+    'и следа нет',
+    'ничуть'
+]
+
+if_wrong = [
+    'CAACAgIAAxkBAAEBNO5ghFTPJg2g7EblpZ8_o2gIk-yI3wACmwAD9wLID687ftTB4dOYHwQ',
+    'CAACAgIAAxkBAAEBNOtghFTKAtBd1kCUKSfYyy5rE7LzagACBgEAAvcCyA83vT1QSAABY5IfBA',
+    'CAACAgIAAxkBAAEBNOhghFS8YTJlQAmSJbT4hGerSju0ygACugkAAvFCvwVQOmZw_D2Rbh8E',
+    'CAACAgIAAxkBAAEBNOVghFS4psDBt0IHdu1W2kXqre_-aAACSwADKA9qFNoPqQbjuZ_LHwQ',
+    'CAACAgIAAxkBAAEBNN9ghFSvqhD8zhsvuerzCg9lD4tm6AAC-AIAAvPjvgtEczIS2SZ-Uh8E',
+    'CAACAgIAAxkBAAEBNNxghFSIb_BK1vr7qPlnO4AdJpDpGwACRAADKA9qFFts_7_cyqtAHwQ',
+    'CAACAgIAAxkBAAEBNNlghFSC-D1cKtSm7Ns4A503PZDsPwACJgAD0D_tD0KVCBQlxt4ZHwQ',
+    'CAACAgIAAxkBAAEBNNZghFSAwhnmzL7BlTcxC5hSJi7QUAACXQEAAtA0Oie5Hgonj3Fv2h8E',
+    'CAACAgIAAxkBAAEBNNNghFR7zsYxZl9xvJdPQxxf_6GXIwACdwEAAhZCawrTguE999zNwx8E',
+    'CAACAgIAAxkBAAEBNNBghFRrNgQcSh3waLAonefVaRLobwACEAUAAs9fiweKscZVYpoFIh8E',
+    'CAACAgIAAxkBAAEBNPFghFU3_7B-udXxWndBBA2w_7-urAACZwAD5KDOB6GjeJZ2Piz9HwQ',
+    'CAACAgIAAxkBAAEBNPRghFU99H6qcPJLK9-VBlzWXghaXgACXQUAAiMFDQABdklXYVN8a8gfBA'
+]
---
+++ questions.py
@@ -0,0 +1,7 @@
+for_ask = {'https://cf2.ppt-online.org/files2/slide/w/ws1OkbxauXHRm7IfJ0Eno6BKAStjGTQNci35dFlU4e/slide-2.jpg': 'а',
+           'https://cf2.ppt-online.org/files2/slide/w/ws1OkbxauXHRm7IfJ0Eno6BKAStjGTQNci35dFlU4e/slide-3.jpg': 'б',
+           'https://cf2.ppt-online.org/files2/slide/w/ws1OkbxauXHRm7IfJ0Eno6BKAStjGTQNci35dFlU4e/slide-4.jpg': 'а',
+           'https://cf2.ppt-online.org/files2/slide/w/ws1OkbxauXHRm7IfJ0Eno6BKAStjGTQNci35dFlU4e/slide-5.jpg': 'б',
+           'https://cf2.ppt-online.org/files2/slide/w/ws1OkbxauXHRm7IfJ0Eno6BKAStjGTQNci35dFlU4e/slide-6.jpg': 'в',
+           'https://cf2.ppt-online.org/files2/slide/w/ws1OkbxauXHRm7IfJ0Eno6BKAStjGTQNci35dFlU4e/slide-7.jpg': 'в',
+           'https://cf2.ppt-online.org/files2/slide/w/ws1OkbxauXHRm7IfJ0Eno6BKAStjGTQNci35dFlU4e/slide-8.jpg': 'б'}
